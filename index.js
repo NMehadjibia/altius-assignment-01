3c11f64d16a9644cb57e997deadca33a8b63f855
NMehadjibia <nourelhoudamehadjibia@gmail.com>
2022-04-06 02:19:17 +0100

    feat: try and catch while reading inputs

--- index.js
+++ index.js
@@ -17,63 +17,62 @@ async function main(){
     });
     const inputsIterator = inputsReadLineStream[Symbol.asyncIterator]();
 
-    const outputsFolderName = "Outputs";
-    const outputsFileName = inputsFileName + "-outputs";
-    const outputsFilePath = path.join(__dirname, outputsFolderName, outputsFileName);
+    let testCases = [];
 
-    const outputsWriteStream = fs.createWriteStream(outputsFilePath, {
-        flags: 'a', encoding: 'utf8'
-    });
-
-    let tests = [];
-    let list1SizeLine = await inputsIterator.next();
+    try{
+        let list1SizeLine = await inputsIterator.next();
         
-    while (!list1SizeLine.done) {
-        let list1Head;
-        let previousNode;
-        let currentNode;
+        while (!list1SizeLine.done) {
+            let list1Head;
+            let previousNode;
+            let currentNode;
 
-        let list1Size = parseInt(list1SizeLine.value);
-        for (let i = 0; i < list1Size; i ++){
-            let line = await inputsIterator.next();
-            currentNode = new SinglyLinkedListNode(parseInt(line.value), null);
-            if (i === 0) list1Head = currentNode;
-            else previousNode.next = currentNode;
-            previousNode = currentNode;
+            let list1Size = parseInt(list1SizeLine.value);
+            for (let i = 0; i < list1Size; i ++){
+                let line = await inputsIterator.next();
+                currentNode = new SinglyLinkedListNode(parseInt(line.value), null);
+                if (i === 0) list1Head = currentNode;
+                else previousNode.next = currentNode;
+                previousNode = currentNode;
+            }
+
+            let list2Head;
+
+            let list2SizeLine = await inputsIterator.next();
+            let list2Size = parseInt(list2SizeLine.value);
+            for (let i = 0; i < list2Size; i ++){
+                let line = await inputsIterator.next();
+                currentNode = new SinglyLinkedListNode(parseInt(line.value), null);
+                if (i === 0) list2Head = currentNode;
+                else previousNode.next = currentNode;
+                previousNode = currentNode;
+            }
+            testCases.push({ list1Head, list2Head });
+            list1SizeLine = await inputsIterator.next();
         }
-
-        let list2Head;
-
-        let list2SizeLine = await inputsIterator.next();
-        let list2Size = parseInt(list2SizeLine.value);
-        for (let i = 0; i < list2Size; i ++){
-            let line = await inputsIterator.next();
-            currentNode = new SinglyLinkedListNode(parseInt(line.value), null);
-            if (i === 0) list2Head = currentNode;
-            else previousNode.next = currentNode;
-            previousNode = currentNode;
-        }
-        tests.push({ list1Head, list2Head });
-        list1SizeLine = await inputsIterator.next();
+    } catch (error) {
+        console.log("Something went wrong while processing inputs !");
+    } finally {
+        inputsReadLineStream.close();
+        inputsReadStream.close();
     }
-        
-    for (let test of tests){
+       
+    for (let test of testCases){
         let result = compareLists(test.list1Head, test.list2Head);
         console.log(result);
-        outputsWriteStream.write(`${result}\n`);
     }
 }
 
 function compareLists(list1Head, list2Head) {
     let equal = true;
-    let endList1 = list1Head === null;
-    let endList2 = list2Head === null;
+    let endList1 = !list1Head;
+    let endList2 = !list2Head;
     while (equal && !endList1 && !endList2) {
         equal = list1Head.data === list2Head.data;
         list1Head = list1Head.next;
         list2Head = list2Head.next;
-        endList1 = list1Head === null;
-        endList2 = list2Head === null;
+        endList1 = !list1Head;
+        endList2 = !list2Head;
     }
     return equal && endList1 && endList2 ? 1 : 0;
 }
